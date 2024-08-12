4fb2c008ad132c5dad83e50145667b4eeea987ed
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2024-08-12 17:41:16 +0200

    fix: Yubi pin, slot, pin-policy.

--- scripts/yk-sign-int.sh
+++ scripts/yk-sign-int.sh
@@ -6,8 +6,8 @@ OPENSSL_BIN=`which openssl`
 #OPENSSL_ENGINE="engine dynamic -pre SO_PATH:/usr/local/lib/engines/engine_pkcs11.so -pre ID:pkcs11 \
 #    -pre LIST_ADD:1 -pre LOAD -pre MODULE_PATH:/usr/local/lib/opensc-pkcs11.so"
 
-if [ "$#" -ne 2 ]; then 
-    echo "Usage: $0 DIR ROOT_NAME INT_NAME"
+if [ "$#" -ne 3 ]; then 
+    echo "Usage: $0 <ROOT_NAME> <INT_NAME> <PIV_PIN_FILE>"
     exit
 fi
 
@@ -15,6 +15,7 @@ fi
 export ROOT_NAME=$1
 export INT_NAME=$2
 export DIR=$ROOT_NAME
+export PIN=$(<$3)
 
 set -e
 
@@ -35,8 +36,8 @@ fi
 echo "Signing intermediate certificate: $DIR/$INT_NAME.crt with $DIR/$ROOT_NAME.crt"
 
 # Run sign command
-OPENSSL_CONF=$CONFIG openssl x509 -engine pkcs11 -CAkeyform engine -CAkey slot_0-id_2 \
-    -sha512 -CA $DIR/$ROOT_NAME.crt -req -extensions v3_ca -extfile $DIR/$INT_NAME.conf -days=$EXPIRY_DAYS \
+OPENSSL_CONF=$CONFIG openssl x509 -engine pkcs11 -CAkeyform engine -CAkey "pkcs11:id=%02;type=private;pin-value=$PIN" \
+    -sha512 -CA $DIR/$ROOT_NAME.crt -req -extensions v3_ca -extfile $DIR/$INT_NAME.conf -days=$INT_EXPIRY_DAYS \
     -in $DIR/$INT_NAME.csr -out $DIR/$INT_NAME.crt
 
 echo "Signed $DIR/$INT_NAME.crt"
